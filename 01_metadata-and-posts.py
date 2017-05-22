bf3ec362d387b78e79bfe03f5d66601aad8cb453
creaciond <daria.maximova.m@gmail.com>
2017-05-22 14:04:21 +0300

    edit gitingore v2
    точечки всему мешали

--- 01_metadata-and-posts.py
+++ 01_metadata-and-posts.py
@@ -2,6 +2,7 @@ import requests
 import os
 import re
 import html
+import time
 
 '''
                 COMMON METHODS
@@ -10,7 +11,7 @@ import html
 def clean_line(line):
     regTag = re.compile('<.*?>', flags=re.DOTALL)
     regLink = re.compile('https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,}', flags=re.DOTALL)
-    regName = re.compile('\[id[0-9]+?|.*?\], ', flags=re.DOTALL)
+    regName = re.compile('\[id[0-9]+?\|.*?\], ', flags=re.DOTALL)
     regHashtag = re.compile('#.? ', flags=re.DOTALL)
     line = regTag.sub('', line)
     line = regLink.sub('', line)
@@ -126,11 +127,11 @@ def api_comments(set_comments, params, offset):
         for i in range(1, len(response.json()['response'])):
             comment = response.json()['response'][i]
             '''
-                group_id = params['owner_id']
+                group id = params['owner_id']
                 from whom is the comment — comment['from_id']
                 text of the comment — comment['text']
             '''
-            line = params['owner_id'] + '\t' + str(comment['from_id']) + '\t' + comment['text']
+            line = params['owner_id'].strip('-') + '\t' + str(comment['from_id']) + '\t' + clean_line(comment['text'])
             set_comments.add(line)
     return set_comments
 
@@ -146,12 +147,17 @@ def get_comments(group_info):
         count += 1
         parameters['post_id'] = post_id
         response = requests.get('https://api.vk.com/method/wall.getComments', params=parameters)
-        if response.json()['response'][0] > 100:
-            for i in range(response.json()['response'][0] % 100 + 1):
-                comments = api_comments(comments, parameters, i*100)
-        else:
-            # offset = 0
-            comments = api_comments(comments, parameters, 0)
+        # status_code 200 = ok
+        if response.status_code == 200:
+            print('post %d, comments length: %s, percentage done: %f per cent' % (
+                count, response.json()['response'][0], count / len(post_ids) * 100))
+            print(str(time.ctime()))
+            if response.json()['response'][0] > 100:
+                for i in range(response.json()['response'][0] % 100 + 1):
+                    comments = api_comments(comments, parameters, i*100)
+            else:
+                # offset = 0
+                comments = api_comments(comments, parameters, 0)
     save_new_info('./data/' + group_info['domain'] + '_comments.tsv', comments)
 
 
@@ -164,7 +170,7 @@ def main():
     groups_meta = []
     groups_meta = group_info(domains, groups_meta)
     for group_meta in groups_meta:
-        write_posts(group_meta)
+        # write_posts(group_meta)
         get_comments(group_meta)
 
 
